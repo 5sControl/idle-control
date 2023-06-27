4b9efd4ac5b33ee46d262bf7c19f5c8e64dff67f
eugenos-programos <elena.samchuk.73@mail.ru>
2023-06-27 10:51:25 +0300

    fix(configs): separate confidence thresholds

--- confs/load_configs.py
+++ confs/load_configs.py
@@ -3,7 +3,8 @@ import json
 
 with open("confs/configs.json", "r") as conf:
     configs = json.load(conf)
-    CONF_THRES = configs.get("conf_thres")
+    CONF_THRES_1 = configs.get("conf_thres_1")
+    CONF_THRES_2 = configs.get("conf_thres_2")
     IOU_THRES = configs.get("iou_thres")
     FIRST_MODEL_PATH = configs.get("first_model_path")
     SECOND_MODEL_PATH = configs.get("second_model_path")
